d02fe42051be6ceda8268671dcf8a41cfc4d256b
Mithil <mithil467@gmail.com>
2019-09-07 21:55:51 +0530

    Puzzle wallbreaker almost done

--- Assets/Scripts/WallSetter.cs
+++ Assets/Scripts/WallSetter.cs
@@ -1,16 +1,73 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-public class WallSetter : MonoBehaviour {
+public class WallSetter : MonoBehaviour
+{
+    private GameObject[] holeFiller = new GameObject[4];
+    public List<GameObject> bricksList = new List<GameObject>();
+    private List<int> done = new List<int>();
+    int index = 0, j = 0;
+    private List<int> soln = new List<int>() { 1, 3, 6, 0 };
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 1; i < 9; ++i)
+            bricksList[i].GetComponent<cakeslice.Outline>().enabled = false;
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void checkSolution()
+    {
+        if (done.SequenceEqual(soln))
+            Collapse();
+        else
+            Debug.Log("Fail");
+    }
+
+    void Collapse()
+    {
+        Debug.Log("BOOM!");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            holeFiller[j] = bricksList[index];
+            done.Add(index);
+            if (j == 0)
+            {
+                holeFiller[0].transform.position = new Vector3(0, 0.5f, 0);
+            }
+            else if (j == 1)
+            {
+                holeFiller[1].transform.position = new Vector3(1, 0.5f, 0);
+            }
+            else if (j == 2)
+            {
+                holeFiller[2].transform.position = new Vector3(0, 0, 0);
+            }
+            else if (j == 3)
+            {
+                holeFiller[3].transform.position = new Vector3(1, 0f, 0);
+                checkSolution();
+            }
+            ++j;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            bricksList[index].GetComponent<cakeslice.Outline>().enabled = false;
+            index = ++index % (bricksList.Count);
+            if (done != null && done.Contains(index))
+                index = ++index % (bricksList.Count);
+            if (done != null && done.Contains(index))
+                index = ++index % (bricksList.Count);
+            if (done != null && done.Contains(index))
+                index = ++index % (bricksList.Count);
+            if (done != null && done.Contains(index))
+                index = ++index % (bricksList.Count);
+            bricksList[index].GetComponent<cakeslice.Outline>().enabled = true;
+        }
+    }
 }
