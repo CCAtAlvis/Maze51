759bc3e01fdd9da82deb7c8ebf4ce9e67376a182
Aakash1006 <aakashbapuchavan@gmail.com>
2019-09-10 14:48:49 +0530

    added 2 puzzels

---
+++ Assets/Scripts AC/Invisible.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Invisible : MonoBehaviour {
+
+    public GameObject text1;
+
+    public static string s1,s2;
+
+    static int temp;
+
+    void Start()
+    {
+        //temp = Random.Range(0, 4);
+        temp = 0;
+
+        switch (temp)
+        {
+            case 0:
+                s1=text1.GetComponent<TextMesh>().text = "When I grow up, I want to be a ranger.";
+                s2 = "When I grow up, I want to           anger.";
+                break;
+            case 1:
+                s1=text1.GetComponent<TextMesh>().text = "I agree not to eat all the food.";
+                s2 = "I a           ot to eat all the food.";
+                break;
+            case 2:
+                s1=text1.GetComponent<TextMesh>().text = "Bob decided to jump in, knowing that this is his only choice.";
+                s2 = "Bob decided to jum          nowing that this is his only choice.";
+                break;
+            case 3:
+                s1=text1.GetComponent<TextMesh>().text = "The sunset makes Cairo seem magnificent.";
+                s2 = "The sunset makes Cai         em magnificent.";
+                break;
+
+            default:
+                Debug.Log("failed randomizing");
+                break;
+
+        }
+
+
+    }
+
+     void OnMouseOver()
+    {
+        text1.GetComponent<TextMesh>().text=s2;
+    }
+
+     void OnMouseExit()
+    {
+        text1.GetComponent<TextMesh>().text = s1;
+    }
+}
---
+++ Assets/Scripts AC/pathfollow.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pathfollow : MonoBehaviour {
+
+    public GameObject[] block;
+
+    public static int x=0, z=0,c=0,ch,index=0;
+
+    public static int[] xrand, zrand,answerkey; 
+
+    public int scale=1;
+
+    //block = new GameObject[4];
+	// Use this for initialization
+	void Start () {
+
+        //block = new GameObject[4];
+        xrand = new int[4];
+        zrand = new int[4];
+
+
+        for (int i = 0; i < 4; i++)
+        {
+            xrand[i] = Random.Range(x, x+2);
+            zrand[i] = Random.Range(z, z+2);
+
+            block[i].transform.position = new Vector3(xrand[i]*scale, 0, zrand[i]*scale);
+
+            switch (i)
+            {
+                case 0:
+                    x = x + 2;
+                    break;
+                case 1:
+                    x = x - 2;
+                    z = z + 2;
+                    break;
+                case 2:
+                    x = x + 2;
+                    z = z + 2;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        //answerkey = new int[4];
+
+
+
+        if (zrand[0] == zrand[1] && zrand[2] == zrand[3] && xrand[0] == xrand[2] && xrand[1] == xrand[3])
+            ch = 0;
+        else if ((zrand[0] == zrand[1] && xrand[0] == xrand[2]) || (zrand[0] == zrand[1] && xrand[1] == xrand[3]) || (zrand[2] == zrand[3] && xrand[2] == xrand[0]) || (zrand[2] == zrand[3] && xrand[1] == xrand[3]))
+            ch = 1;
+        else if ((zrand[0] == zrand[1] && zrand[2] == zrand[3]) || (xrand[0] == xrand[2] && xrand[1] == xrand[3]))
+            ch = 2;
+        else if (zrand[0] == zrand[1] || zrand[2] == zrand[3] || xrand[0] == xrand[2] || xrand[1] == xrand[3])
+            ch = 3;
+        else
+            ch = 4;
+
+        switch(ch)
+        {
+            case 0:
+                answerkey =new int[]{ 0,1,2,3};
+                break;
+            case 1:
+                answerkey =new int[]{1,3,2,0};
+                break;
+            case 2:
+                answerkey =new int[]{0,2,3,1};
+                break;
+            case 3:
+                answerkey =new int[]{0,1,3,2};
+                break;
+            default:
+                answerkey =new int []{ 0,3,2,4};
+                break;
+        }
+        Debug.Log(ch);
+	}
+
+    
+
+
+}
