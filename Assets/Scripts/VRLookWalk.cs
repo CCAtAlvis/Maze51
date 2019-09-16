67e23106b74bb76f7e16755bee42aad807803761
Mithil <mithil467@gmail.com>
2019-09-16 22:07:40 +0530

    Added VR movement
    Signed-off-by: Mithil <mithil467@gmail.com>

---
+++ Assets/Scripts/VRLookWalk.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRLookWalk : MonoBehaviour
+{
+    public Transform cameraTransform;
+    public float toggleAngle = 10.0F;
+    public float speed = 3.0F;
+    public bool moveForward;
+
+    private CharacterController characterController;
+
+    void Start()
+    {
+        characterController = GetComponentInParent<CharacterController>();
+        cameraTransform = Camera.main.transform;
+    }
+
+    void Update()
+    {
+
+        if (cameraTransform.eulerAngles.x >= toggleAngle
+          && cameraTransform.eulerAngles.x <= 90.0f)
+            moveForward = true;
+        else
+            moveForward = false;
+
+        if (moveForward)
+        {
+            Vector3 forward = cameraTransform.TransformDirection(Vector3.forward);
+            characterController.SimpleMove(forward * speed * toggleAngle / 5);
+        }
+    }
+}
