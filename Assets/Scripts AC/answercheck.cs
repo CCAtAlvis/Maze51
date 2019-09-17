aea926a81de0b6a9320aaa780e8924f9b1649e69
Aakash1006 <aakashbapuchavan@gmail.com>
2019-09-17 17:52:15 +0530

    added all

--- Assets/Scripts AC/answercheck.cs
+++ Assets/Scripts AC/answercheck.cs
@@ -6,7 +6,11 @@ public class answercheck : MonoBehaviour {
 
     public GameObject button;
 
+    public GameObject trig;
+
     public static int direction;
+
+    public int buttonno;
 
     public static int flag=0;
     //public bool 
@@ -17,29 +21,21 @@ public class answercheck : MonoBehaviour {
     {
         if (flag == 0) { 
         direction = swaper.num;
-        button.transform.localScale -= new Vector3(0, 0, 0.5f);
+        //button.transform.localScale -= new Vector3(0, 0, 0.5f);
         float pos = button.transform.position.x;
             //Debug.Log(button.transform.position.x);
 
-            if (pos < 0 && direction == 0)
+            if (direction == buttonno)
             {
                 Debug.Log("Success");
                 flag = 1;
-            }
-            else if (pos == 0 && direction == 1)
-            {
-                Debug.Log("Success");
-                flag = 1;
-            }
-            else if (pos > 0 && direction == 2)
-            {
-                Debug.Log("Success");
-                flag = 1;
+                trig.GetComponent<PuzzleController>().PuzzleSolved(5);
             }
             else
             {
                 Debug.Log("Failed");
                 flag = 1;
+                trig.GetComponent<PuzzleController>().PuzzleFailed(5);
             }
                 
         }
